32563ed8264e635db657e4caec5461c8744a4d28
bhowe111 <brendan.howe1@gmail.com>
2021-03-07 22:00:57 -0800

    created category badges

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,20 +1,77 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, ActivityIndicator, FlatList } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import { Container, Header, Icon, Item, Input, Text } from "native-base";
 
 import ProductList from "./ProductList";
+import SearchedProduct from "./SearchedProducts";
+import Banner from "../../Shared/Banner";
+import CategoryFilter from "./CategoryFilter";
 
+var { height } = Dimensions.get("window");
 const data = require("../../assets/products.json");
+const productsCategories = require("../../assets/categories.json");
 
 const ProductContainer = () => {
   const [products, setProducts] = useState([]);
+  const [productsFiltered, setProductsFiltered] = useState([]);
+  const [focus, setFocus] = useState();
+  const [categories, setCategories] = useState([]);
+  const [productsCtg, setProductsCtg] = useState([]);
+  const [active, setActive] = useState();
+  const [initialState, setInitialState] = useState([]);
+
   useEffect(() => {
     setProducts(data);
+    setProductsFiltered(data);
+    setFocus(false);
+    setCategories(productsCategories);
+    setActive(-1);
+    setInitialState(data);
 
     return () => {
       setProducts([]);
+      setProductsFiltered([]);
+      setFocus();
+      setCategories([]);
+      setActive();
+      setInitialState();
     };
   }, []);
+
+  const searchProduct = (text) => {
+    setProductsFiltered(
+      products.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
+    );
+  };
+
+  const openList = () => {
+    setFocus(true);
+  };
+
+  const onBlur = () => {
+    setFocus(false);
+  };
+
+  // Categories
+  const changeCtg = (ctg) => {
+    {
+      ctg === "all"
+        ? [setProductsCtg(initialState), setActive(true)]
+        : [
+            setProductsCtg(
+              products.filter((i) => i.category._id === ctg),
+              setActive(true)
+            ),
+          ];
+    }
+  };
+
   return (
     <Container>
       <Header searchBar rounded>
@@ -22,23 +79,60 @@ const ProductContainer = () => {
           <Icon name="ios-search" />
           <Input
             placeholder="Search"
-            // onFocus={}
-            //onChangeText={(text) => }
+            onFocus={openList}
+            onChangeText={(text) => searchProduct(text)}
           />
+          {focus == true ? <Icon onPress={onBlur} name="ios-close" /> : null}
         </Item>
       </Header>
-      <View>
-        <Text>Product Container</Text>
-        <View style={{ marginTop: 100 }}>
-          <FlatList
-            data={products}
-            renderItem={({ item }) => <ProductList key={item.id} item={item} />}
-            keyExtractor={(item) => item.name}
-          />
+      {focus == true ? (
+        <SearchedProduct productsFiltered={productsFiltered} />
+      ) : (
+        <View>
+          <View>
+            <Banner />
+            <View>
+              <CategoryFilter
+                categories={categories}
+                categoryFilter={changeCtg}
+                productsCtg={productsCtg}
+                active={active}
+                setActive={setActive}
+              />
+            </View>
+          </View>
+          <View style={{ marginTop: 100 }}>
+            <FlatList
+              data={products}
+              renderItem={({ item }) => (
+                <ProductList key={item.id} item={item} />
+              )}
+              keyExtractor={(item) => item.name}
+            />
+          </View>
         </View>
-      </View>
+      )}
     </Container>
   );
 };
 
 export default ProductContainer;
+
+const styles = StyleSheet.create({
+  container: {
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  listContainer: {
+    height: height,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
